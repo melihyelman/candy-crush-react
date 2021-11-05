b1da7db0412a771302cb3c30d74e008d05395e84
melihyelman <myelman17@gmail.com>
2021-11-05 21:56:09 +0300

    add new features

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Modal from "react-modal";
 
 import BlueCandy from "./img/blue-candy.png";
 import RedCandy from "./img/red-candy.png";
@@ -28,14 +29,35 @@ const candyColors = [
   YellowCandy,
 ];
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    width: "75%",
+    height: "75%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement("#root");
+
 export const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
   const [source, setSource] = useState(null);
   const [destination, setDestination] = useState(null);
   const [score, setScore] = useState(0);
+  const [countTouch, setCountTouch] = useState(0);
+  const [users, setUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const colorArrangement = loadBoard(width, candyColors, () => setScore(0));
+    fetch("https://6183021b91d76c00172d172d.mockapi.io/users")  .then(response => response.json())
+  .then(data => setUsers(data)).catch((error) => {
+  console.error('Error:', error);
+});
 
     setCurrentColorArrangement(colorArrangement);
   }, []);
@@ -95,16 +117,16 @@ export const App = () => {
 
   useEffect(() => {
     const timer = setInterval(async () => {
-      await checkForColumnOfFour(width, currentColorArrangement, () =>
+      checkForColumnOfFour(width, currentColorArrangement, () =>
         setScore(() => score + 4)
       );
-      await checkForRowOfFour(currentColorArrangement, () =>
+      checkForRowOfFour(currentColorArrangement, () =>
         setScore(() => score + 4)
       );
-      await checkForColumnOfThree(width, currentColorArrangement, () =>
+      checkForColumnOfThree(width, currentColorArrangement, () =>
         setScore(() => score + 3)
       );
-      await checkForRowOfThree(currentColorArrangement, () =>
+      checkForRowOfThree(currentColorArrangement, () =>
         setScore(() => score + 3)
       );
       await moveIntoSquareBelow(width, candyColors, currentColorArrangement);
@@ -120,17 +142,146 @@ export const App = () => {
     moveIntoSquareBelow,
     currentColorArrangement,
   ]);
+  useEffect(() => {
+    sessionStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
+
+  const addUser = (user) => {
+    fetch('https://6183021b91d76c00172d172d.mockapi.io/users', {
+  method: 'POST', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(user),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+  }
+
+  const handleRestart = () => {
+    const name = window.prompt("Enter your name")
+    setUsers([
+      ...users,
+      { name: name, score: score },
+    ]);
+    addUser({ name: name, score: score })
+    const colorArrangement = loadBoard(width, candyColors, () => setScore(0));
+
+    setCurrentColorArrangement(colorArrangement);
+  };
+  const handleTouch = (e) => {
+    if (countTouch === 0) {
+      setSource(e.target);
+      setCountTouch(1);
+    } else if (countTouch === 1) {
+      const destinationId = parseInt(e.target.getAttribute("data-id"));
+      const sourceId = parseInt(source.getAttribute("data-id"));
+
+      const validMoves = [
+        sourceId - 1,
+        sourceId + 1,
+        sourceId - width,
+        sourceId + width,
+      ];
+
+      const validMove = validMoves.includes(destinationId);
+
+      if (validMove) {
+        currentColorArrangement[destinationId] = source.getAttribute("src");
+        currentColorArrangement[sourceId] = e.target.getAttribute("src");
+      }
+
+      const isARowOfThree = checkForRowOfThree(
+        currentColorArrangement,
+        () => {}
+      );
+      const isAColumnOfThree = checkForColumnOfThree(
+        width,
+        currentColorArrangement,
+        () => {}
+      );
+      const isAColumnOfFour = checkForColumnOfFour(
+        width,
+        currentColorArrangement,
+        () => {}
+      );
+      const isARowOfFour = checkForRowOfFour(currentColorArrangement, () => {});
+
+      if (
+        destinationId &&
+        validMove &&
+        (isAColumnOfFour || isAColumnOfThree || isARowOfFour || isARowOfThree)
+      ) {
+        setCountTouch(0);
+        setSource(null);
+      } else {
+        currentColorArrangement[destinationId] = e.target.getAttribute("src");
+        currentColorArrangement[sourceId] = source.getAttribute("src");
+        setCountTouch(0);
+        setSource(null);
+        setCurrentColorArrangement([...currentColorArrangement]);
+      }
+    }
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="app">
       <div className="score">
         <h1>Score: {score}</h1>
+        <div>
+          <button className="btn" onClick={() => handleRestart()}>
+            Restart
+          </button>
+          <button className="btn user" onClick={openModal}>
+            Users
+          </button>
+          <Modal
+            isOpen={isModalOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <button className="btn" onClick={closeModal}>
+              close
+            </button>
+            <ul className="userList">
+              <li>Name - Score</li>
+              {users.length > 0  &&
+                users
+                  .sort((first, second) => second.score - first.score)
+                  .map((user) => (
+                    <li>
+                      <span>{user.name}</span>
+                      <span>{user.score}</span>
+                    </li>
+                  ))}
+            </ul>
+          </Modal>
+        </div>
       </div>
       <div className="game">
         {currentColorArrangement.map((candyColor, index) => (
           <img
             key={index}
             src={candyColor}
+            className={
+              index === parseInt(source?.getAttribute("data-id"))
+                ? "selected"
+                : ""
+            }
             alt={candyColor}
             data-id={index}
             draggable={true}
@@ -140,6 +291,7 @@ export const App = () => {
             onDragLeave={(e) => e.preventDefault()}
             onDrop={dragDrop}
             onDragEnd={dragEnd}
+            onTouchEnd={handleTouch}
           />
         ))}
       </div>
